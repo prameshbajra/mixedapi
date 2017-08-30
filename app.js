17db85415f3fd20738f618cd4de9af3f79a166e8
Suzal3579 <pe.messh@gmail.com>
2017-08-31 00:06:15 +0530

    Naming done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
+const jokes = require('./routes/jokes');
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', jokes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
